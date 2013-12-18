364b4deb10e1be026d46bc02536c7f116f86c685
Michael Parkhill <mike@silverstripe.com>
2013-12-19 11:44:39 +1300

    fixes issue 1 - csv export file is empty, code comments updated too

--- code/RedirectedURLAdmin.php
+++ code/RedirectedURLAdmin.php
@@ -1,14 +1,26 @@
 <?php
-
+/**
+ * Provides CMS Administration of {@link: RedirectedURL} objects
+ *
+ * @package redirectedurls
+ * @author sam@silverstripe.com
+ * @author scienceninjas@silverstripe.com
+ */
 class RedirectedURLAdmin extends ModelAdmin {
-	static $url_segment = 'redirects';
-	static $menu_title = 'Redirects';
-	
-	static $managed_models = array(
+
+	private static $url_segment = 'redirects';
+	private static $menu_title = 'Redirects';
+
+	private static $managed_models = array(
 		'RedirectedURL',
 	);
-	
-	function getModelImporters() {
+
+	public function init() {
+		parent::init();
+		Requirements::javascript('mysite/javascript/AdminCenterColumnWide.js');
+	}
+
+	public function getModelImporters() {
 		$importer = new CsvBulkLoader("RedirectedURL");
 		$importer->duplicateChecks = array(
 			'FromBase' => array('callback' => 'findByFrom'),
@@ -17,25 +29,20 @@ class RedirectedURLAdmin extends ModelAdmin {
 			'RedirectedURL' => $importer
 		);
 	}
-	
+
 	/**
-	 * Overriden so that the CSV column is exactly as the output 
-	 * 
-	 * DataObject::summary_fields() get this wrong by separate words, ie FromBase => From Base which confuses the 
-	 * importer later.
-	 * 
+	 * Overriden so that the CSV column headings have the exact field names of the DataObject
+	 *
+	 * To prevent field name conversion in DataObject::summaryFields() during export
+	 * e.g. 'FromBase' is output as 'From Base'
+	 *
 	 * @return array
 	 */
 	public function getExportFields() {
-		return array(
-			"FromBase",
-			"FromQuerystring",
-			"To",
-		);
-	}
-
-	public function init() {
-		parent::init();
-		Requirements::javascript('mysite/javascript/AdminCenterColumnWide.js');
+		$fields = array();
+		foreach(singleton($this->modelClass)->db() as $field => $spec) {
+			$fields[$field] = $field;
+		}
+		return $fields;
 	}
 }
