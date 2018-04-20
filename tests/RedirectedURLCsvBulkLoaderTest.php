1dbaea2b233160fb0410fe90b966439fca312fce
priyashantha <priyashantha@silverstripers.com>
2018-04-20 11:22:49 +0530

    unit test added for testDuplicateFromImport

---
+++ tests/RedirectedURLCsvBulkLoaderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class RedirectedURLCsvBulkLoaderTest extends SapphireTest {
+
+    protected static $fixture_file = 'RedirectedURLCsvBulkLoaderTest.yml';
+
+    public function testDuplicateFromImport() {
+        $loader = new RedirectedURLCsvBulkLoader('RedirectedURL');
+        $results = $loader->load($this->getCurrentRelativePath() . '/RedirectedURLCsvBulkLoaderTest.csv');
+        $created = $results->Created()->toArray();
+        $this->assertEquals(count($created), 2);
+
+        $this->assertEquals($created[0]->FromBase, '/about-us/duplicated-from-base.html');
+        $this->assertEquals($created[0]->FromQuerystring, 'duplicated-query-string=1');
+
+        $this->assertEquals($created[1]->FromBase, '/example/with-querystring.html');
+        $this->assertEquals($created[1]->FromQuerystring, 'foo=1');
+    }
+
+}
