ae1a042fa085c33f92e4a20b1f3e72b905ab6ab9
Loz Calver <lozcalver@bigfork.co.uk>
2018-11-12 10:12:12 +0000

    Add PHP 7.2 support

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
-Object::add_extension("RequestHandler", "RedirectedURLHandler");
-Object::add_extension("ContentController", "RedirectedURLHandler");
-Object::add_extension("ModelAsController", "RedirectedURLHandler");
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
+SS_Object::add_extension("RequestHandler", "RedirectedURLHandler");
+SS_Object::add_extension("ContentController", "RedirectedURLHandler");
+SS_Object::add_extension("ModelAsController", "RedirectedURLHandler");
