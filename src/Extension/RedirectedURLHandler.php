631f035f525e16f160f28cdc7bd240c4b389c97e
Matt Peel <matt@silverstripe.com>
2020-04-27 23:07:35 +1200

    FEAT: Allow redirectedurls to apply to assets

--- src/Extension/RedirectedURLHandler.php
+++ src/Extension/RedirectedURLHandler.php
@@ -11,9 +11,11 @@ use SilverStripe\Control\HTTPResponse_Exception;
 use SilverStripe\Control\RequestHandler;
 use SilverStripe\Core\Convert;
 use SilverStripe\Core\Extension;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\ArrayList;
 use SilverStripe\RedirectedURLs\Model\RedirectedURL;
 use SilverStripe\Core\Config\Config;
+use SilverStripe\RedirectedURLs\Service\RedirectedURLService;
 
 /**
  * Handles the redirection of any url from a controller.
@@ -68,85 +70,15 @@ class RedirectedURLHandler extends Extension
      * @throws HTTPResponse_Exception
      * @param HTTPRequest $request
      */
-    public function onBeforeHTTPError404($request)
+    public function onBeforeHTTPError404(HTTPRequest $request)
     {
-        $base = strtolower($request->getURL());
+        /** @var RedirectedURLService $service */
+        $service = Injector::inst()->get(RedirectedURLService::class);
 
-        $getVars = $this->arrayToLowercase($request->getVars());
+        $match = $service->findBestRedirectedURLMatch($request);
 
-        // Find all the RedirectedURL objects where the base URL matches.
-        // Assumes the base url has no trailing slash.
-        $SQL_base = Convert::raw2sql(rtrim($base, '/'));
-
-        $potentials = RedirectedURL::get()->filter(['FromBase' => '/' . $SQL_base])->sort('FromQuerystring DESC');
-        $listPotentials = new ArrayList();
-        foreach ($potentials as $potential) {
-            $listPotentials->push($potential);
-        }
-
-        // Find any matching FromBase elements terminating in a wildcard /*
-        $baseparts = explode('/', $base);
-        for ($pos = count($baseparts) - 1; $pos >= 0; $pos--) {
-            $basestr = implode('/', array_slice($baseparts, 0, $pos));
-            $basepart = Convert::raw2sql($basestr . '/*');
-            $basepots = RedirectedURL::get()->filter(['FromBase' => '/' . $basepart])->sort('FromQuerystring DESC');
-            foreach ($basepots as $basepot) {
-                // If the To URL ends in a wildcard /*, append the remaining request URL elements
-                if ($basepot->RedirectionType === 'External' && substr($basepot->To, -2) === '/*') {
-                    $basepot->To = substr($basepot->To, 0, -2) . substr($base, strlen($basestr));
-                }
-                $listPotentials->push($basepot);
-            }
-        }
-
-        $matched = null;
-
-        // Then check the get vars, ignoring any additional get vars that
-        // this URL may have
-        if ($listPotentials) {
-            foreach ($listPotentials as $potential) {
-                $allVarsMatch = true;
-
-                if ($potential->FromQuerystring) {
-                    $reqVars = array();
-                    parse_str($potential->FromQuerystring, $reqVars);
-
-                    foreach ($reqVars as $k => $v) {
-                        if (!$v) {
-                            continue;
-                        }
-
-                        if (!isset($getVars[$k]) || $v != $getVars[$k]) {
-                            $allVarsMatch = false;
-                            break;
-                        }
-                    }
-                }
-
-                if ($allVarsMatch) {
-                    $matched = $potential;
-                    break;
-                }
-            }
-        }
-
-        // If there's a match, direct!
-        if ($matched) {
-            $response = new HTTPResponse();
-            $dest = $matched->Link();
-            $response->redirect(Director::absoluteURL($dest), $this->getRedirectCode($matched));
-
-            throw new HTTPResponse_Exception($response);
-        }
-
-        // Otherwise check for default MOSS-fixing.
-        if (preg_match('/pages\/default.aspx$/i', $base)) {
-            $newBase = preg_replace('/pages\/default.aspx$/i', '', $base);
-
-            $response = new HTTPResponse();
-            $response->redirect(Director::absoluteURL($newBase), $this->getRedirectCode());
-
-            throw new HTTPResponse_Exception($response);
+        if ($match) {
+            throw new HTTPResponse_Exception($service->getResponse($match));
         }
     }
 }
